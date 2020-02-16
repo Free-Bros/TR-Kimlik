4498344b1a80849fb20487db3e1f25aa4273dfcd
freeman9999 <59989323+freeman9999@users.noreply.github.com>
2020-02-16 05:00:15 +0300

    Add files via upload

---
+++ kimlik2/lua/autorun/client/pass_config.lua
@@ -0,0 +1,21 @@
+--- update soon ---
+--- Time does not stand still ---
+-- :D ---
+surface.CreateFont("PassHud", {
+	font = "Arial",
+	size = 25,
+	weight = 600,
+});
+
+surface.CreateFont("PassHud2", {
+	font = "Arial",
+	size = 15,
+	weight = 600,
+});
+
+surface.CreateFont("PassHud3", {
+	font = "Arial",
+	size = 20,
+	weight = 600,
+});
+
